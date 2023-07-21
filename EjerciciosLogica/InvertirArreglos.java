bed1fb8394679dc104073bcd6d0b2e9fd0faee6f
Bohorquez <Jaelbkz@icloud.com>
2023-07-20 23:42:06 -0600

    s

--- EjerciciosLogica/InvertirArreglos.java
+++ EjerciciosLogica/InvertirArreglos.java
@@ -5,9 +5,13 @@ import java.util.Scanner;
 public class InvertirArreglos {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String arreglo = sc.next();
+         int lerge = sc.nextInt();
+        char palabra[] = new char[lerge];
+        for (int i = 0; i<lerge; i++){
+            char arreglo = sc.next().charAt(0);
+            palabra[i] = arreglo;
+        }
         sc.close();
-        char palabra[] = new char[arreglo.length()];
         //char palabra[] = {'T','E','C','N','M'};
         char palabraInvertida [] = new char[palabra.length];
         for (int i=palabra.length-1; i>=0 ; i--){
