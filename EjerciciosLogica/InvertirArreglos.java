3ede0a28b7cf28d755cbd549385429ae45445215
--global <--global>
2023-07-20 23:28:55 -0600

    a

--- EjerciciosLogica/InvertirArreglos.java
+++ EjerciciosLogica/InvertirArreglos.java
@@ -1,13 +1,14 @@
 package EjerciciosLogica;
 
-//import java.util.Scanner;
+import java.util.Scanner;
 
 public class InvertirArreglos {
     public static void main(String[] args) {
-        // Scanner sc = new Scanner(System.in);
-        // String arreglo = sc.next();
-        // char palabra[] = new char[arreglo];
-        char palabra[] = {'T','E','C','N','M'};
+        Scanner sc = new Scanner(System.in);
+        String arreglo = sc.next();
+        sc.close();
+        char palabra[] = new char[arreglo.length()];
+        //char palabra[] = {'T','E','C','N','M'};
         char palabraInvertida [] = new char[palabra.length];
         for (int i=palabra.length-1; i>=0 ; i--){
             int iterator=0;
