c0d8fcc6d83deb2a3b19205e93fceac5a942088c
Bohorquez <Jaelbkz@icloud.com>
2023-08-04 20:52:48 -0600

    fibo y recursi

--- EjerciciosLogica/Fibonacci.java
+++ EjerciciosLogica/Fibonacci.java
@@ -1,7 +1,22 @@
 package EjerciciosLogica;
 
+import java.util.Scanner;
+
 public class Fibonacci {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numberF = sc.nextInt();
+        sc.close();
+        System.out.println("El numero de fibonnaci es: "+fiboOp(numberF));
         
+    }
+    public static int fiboOp(int number){
+        if (number == 0){
+            return 0;
+        }else if (number == 1){
+            return 1;
+        }else{
+            return fiboOp(number-2)+ fiboOp(number -1 );
+        }
     }
 }
