9d3baebc5f0f26c0e34fa0b325fdcee59525bc58
Bohorquez <Jaelbkz@icloud.com>
2023-08-04 20:12:31 -0600

    a

--- EjerciciosLogica/Factorial.java
+++ EjerciciosLogica/Factorial.java
@@ -1,5 +1,21 @@
 package EjerciciosLogica;
 
+import java.util.Scanner;
+
 public class Factorial {
     
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long num = sc.nextInt();
+        sc.close();
+        //factorialop(num);
+        System.out.println("El factorial de " +num+  " es: "+ factorialop(num));
+    }
+    public static long factorialop(long num){
+        if (num == 0){
+            return 1;
+        }else{
+            return num * (factorialop(num - 1));
+        }
+    }
 }
