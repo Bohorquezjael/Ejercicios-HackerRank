17cc6ed2d6e0c5567e5fa404ae8e4a2a1c4e94ad
Bohorquez <Jaelbkz@icloud.com>
2023-08-17 11:28:38 -0600

    StaticBlock

---
+++ JavaStaticInitializerBlock.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class JavaStaticInitializerBlock {
+    static int b,h; 
+static boolean flag;
+    static {
+        Scanner sc = new Scanner(System.in);
+        b = sc.nextInt();
+        h = sc.nextInt();
+        flag = true;
+        try{
+        if (b <= 0 || h <= 0){
+            flag = false;
+            throw new Exception("Breadth and height must be positive");
+        }
+        }catch(Exception e){
+            System.out.println(e);
+        }
+    }
+    public static void main(String[] args) {
+        
+        if(flag){
+            System.out.println(h*b);
+        }else{
+            
+        }
+    }
+}
