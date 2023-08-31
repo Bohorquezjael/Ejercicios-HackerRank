8e9524d7dfcffa7a3118d338b38799d624d8f0f3
Bohorquez <Jaelbkz@icloud.com>
2023-08-30 18:30:48 -0600

    ss

--- EjerciciosLogica/binarioADecimal.java
+++ EjerciciosLogica/binarioADecimal.java
@@ -8,7 +8,7 @@ public class binarioADecimal {
         Scanner sc = new Scanner(System.in);
         System.out.println("Ingresa el numero binario");
         
-
+        sc.close();
     }
     
 }
