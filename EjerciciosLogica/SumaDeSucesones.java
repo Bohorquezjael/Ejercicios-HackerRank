0961566a439f3e12a92b8cde0c3b0f1e08c8e213
Bohorquez <Jaelbkz@icloud.com>
2023-08-30 18:27:54 -0600

    TareasPromedio

---
+++ EjerciciosLogica/SumaDeSucesones.java
@@ -0,0 +1,18 @@
+package EjerciciosLogica;
+import java.util.Scanner;
+
+public class SumaDeSucesones {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("ingresa el primer valor de la sucesion");
+        int primerValor = sc.nextInt();
+        System.out.println("ingresa el ultimo valor de la sucesion");
+        int ultimoValor = sc.nextInt();
+        int tmp = 0;
+        sc.close();
+        for(int i = primerValor; i <= ultimoValor; i++){
+            tmp += i;
+        }
+        System.out.println("La suma es: " + tmp);
+    }
+}
