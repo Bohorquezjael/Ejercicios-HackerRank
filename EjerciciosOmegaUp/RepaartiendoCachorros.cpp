1ee3507805a0844c2d5dab427367be8490a12130
Bohorquez <Jaelbkz@icloud.com>
2023-10-01 21:50:53 -0600

    cachorrosSolucion

--- EjerciciosOmegaUp/RepaartiendoCachorros.cpp
+++ EjerciciosOmegaUp/RepaartiendoCachorros.cpp
@@ -1,16 +1,13 @@
 #include <iostream>
 using namespace std;
-//! PENDIENTE
+
 int main(){
 
-    int c;
-    int p;
-    int h;
-    cin >> c;
-    cin >> p;
-    cin >> h;
-    
-    cout << (p) + (c - p - h - 1) << endl;
+    int c, p, h;
+
+    cin >> c >> p >> h;
+
+    cout << (p) + ((c - p) % (h + 1)) << endl;
     
     return 0;
 }
