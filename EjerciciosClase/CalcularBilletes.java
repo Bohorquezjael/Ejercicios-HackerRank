6c04fa3dea212894b66920a08ae23ec209911664
Bohorquez <Jaelbkz@icloud.com>
2023-10-17 20:44:32 -0600

    practica examen

---
+++ EjerciciosClase/CalcularBilletes.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class CalcularBilletes {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingresa el valor del monto");
+        int cantidadDinero = sc.nextInt();
+        int m1, m2, m20, b100, b200, b500;
+
+        b500 = cantidadDinero/500;
+        cantidadDinero %= 500;
+
+        b200 = cantidadDinero/200;
+        cantidadDinero %= 200;
+
+        b100 = cantidadDinero/100;
+        cantidadDinero %= 100;
+
+        m20 = cantidadDinero/20;
+        cantidadDinero %= 20;
+
+        m2 = cantidadDinero/2;
+        cantidadDinero %= 2;
+
+        m1 = cantidadDinero/1;
+
+        // System.out.printf("\n %d Billetes de $500 \n %d Billetes de $200 \n %d Billetes de $100 \n %d Monedas de $20 \n %d Monedas de $2 y %d Monedas de $1", b500, b200, b100, m20, m2, m1);
+    }
+}
---
+++ EjerciciosClase/CalcularBilletes2.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class CalcularBilletes2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingresa el valor del monto");
+        int cantidadDinero = sc.nextInt();
+        System.out.println("Ingresa cuantas denominaciones hay");
+        int numDenominacion = sc.nextInt();
+        for(int i = 0; i <numDenominacion; i++){
+            System.out.print("\n Ingrese la denominación " + (i + 1) + ": ");
+            int deno = sc.nextInt();
+            int cantidadBilletes = cantidadDinero / deno;
+            System.out.printf("\n %d Billetes de %d", cantidadBilletes, deno);
+            cantidadDinero %= deno;
+        }
+
+
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("Ingresa el valor del monto");
+        // int cantidadDinero = sc.nextInt();
+        // sc.close();
+        // int nBilletes = 0;
+        // int denominaciones[]  = {500, 200, 100, 20, 2, 1};
+        // for(int i = 0; i < denominaciones.length; i++){
+        //     nBilletes = cantidadDinero / denominaciones[i];
+        //     System.out.printf("\n %d Billetes de %d", nBilletes, denominaciones[i]);
+        //     cantidadDinero %= denominaciones[i];
+        // }
+
+    }
+}
