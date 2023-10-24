7271839dcc187d4137b5edc2c11069ad313076bb
Bohorquez <Jaelbkz@icloud.com>
2023-10-24 13:03:50 -0600

    ejercicios 23-10 omegaUp

---
+++ EjerciciosOmegaUp/IgnorandoLosUltimosResultados.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class IgnorandoLosUltimosResultados {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.nextLine();
+        String numeros = sc.nextLine();
+        String numbers[] = numeros.split(" ");
+        int k = sc.nextInt();
+        for (int i = 0; i < numbers.length - k; i++) {
+            System.out.print(Integer.parseInt(numbers[i]) + " ");
+        }
+    }
+}
