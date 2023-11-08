7b95f835aecbfef1f32b78161a53915f182d188c
Bohorquez <Jaelbkz@icloud.com>
2023-11-08 17:23:24 -0600

    Problemas 8-11 OmegaUp

--- EjerciciosOmegaUp/AyudandoAUnaDoctora.java
+++ EjerciciosOmegaUp/AyudandoAUnaDoctora.java
@@ -1,7 +1,6 @@
 import java.util.Scanner;
 
 public class AyudandoAUnaDoctora {
-    //FIXME
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
--- EjerciciosOmegaUp/SeleccionDeEstudiantes.java
+++ EjerciciosOmegaUp/SeleccionDeEstudiantes.java
@@ -1,11 +1,11 @@
 import java.util.Scanner;
-
+//FIXME
 public class SeleccionDeEstudiantes {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt(), b = sc.nextInt();
+        long a = sc.nextInt(), b = sc.nextInt();
         sc.close();
-        String str = Integer.toString(a * b);
+        String str = Long.toString(a * b);
         System.out.println(str.charAt(str.length()-1));
     }
 }
