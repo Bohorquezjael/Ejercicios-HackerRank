8fd467a28d4767346f8b6621da4da509f777ad73
Bohorquez <Jaelbkz@icloud.com>
2023-11-19 23:09:00 -0600

    Problemas OmegaUp avance

---
+++ EjerciciosOmegaUp/Parentesis6097.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Parentesis6097 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String linea = sc.nextLine();
+        sc.close();
+        int contadorAbre = 0, contadorCierra = 0;
+        for (int i = 0; i < linea.length(); i++) {
+            var a = linea.charAt(i) == '(' ? contadorAbre++ : contadorCierra++;
+        }
+        System.out.println(contadorAbre == contadorCierra ? "SI" : "NO");
+    }
+}
---
+++ EjerciciosOmegaUp/Promedio153.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Promedio153 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextInt(), b = sc.nextInt();
+        sc.close();
+        System.out.println((b * 2) - a);
+    }
+}
