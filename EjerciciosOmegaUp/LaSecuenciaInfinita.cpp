72f6fecf917740b9b1ad1002c5c64ab0f7c6ea41
Bohorquez <Jaelbkz@icloud.com>
2024-02-14 12:58:05 -0600

    ejercicio terminado

--- EjerciciosOmegaUp/LaSecuenciaInfinita.cpp
+++ EjerciciosOmegaUp/LaSecuenciaInfinita.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
 using namespace std;
 int main(){
-    int n, control = 1;
+    int n, control = 1, vuelta = 0;
     cin >> n;
-        
+    while(n > 0){
+        cout << control << " ";
+        if(control == 5 || (control == 1 && vuelta != 0)){
+            vuelta++;
+        }
+        if(vuelta % 2 == 0){
+            control++;
+        }else{
+            control--;
+        }
+        n--;
+    }
     return 0;
-}//951-394-62-05
+}
